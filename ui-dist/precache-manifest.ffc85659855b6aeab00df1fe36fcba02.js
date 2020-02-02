c9d284d5ee730613f565f41eb42bd21deccb6487
Kevin Griego <Kevin.Greigo@aexp.com>
2020-02-02 15:21:21 -0700

    forgot some files

---
+++ ui-dist/precache-manifest.ffc85659855b6aeab00df1fe36fcba02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e716e521a8fd6bfdeeb060da2bb8ead",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a09cb69716be1e8cf80e",
+    "url": "/static/css/2.b4e9f8a4.chunk.css"
+  },
+  {
+    "revision": "1f36eb6e29607db7fc8d",
+    "url": "/static/css/main.ca9918e8.chunk.css"
+  },
+  {
+    "revision": "a09cb69716be1e8cf80e",
+    "url": "/static/js/2.91ead711.chunk.js"
+  },
+  {
+    "revision": "5cc625d2a9279fc632bcba952193fb4b",
+    "url": "/static/js/2.91ead711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f36eb6e29607db7fc8d",
+    "url": "/static/js/main.c4879cfc.chunk.js"
+  },
+  {
+    "revision": "a7d782f52add9956554c",
+    "url": "/static/js/runtime-main.be703378.js"
+  }
+]);
